62ec437adb242011a07687af737f71ebe9eed282
Naruse-Kazuki <kuroraido@icloud.com>
2020-04-10 23:17:07 +0900

    createTextField

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new FriendlychatApp());
+  runApp(new FriendlyChatApp());
 }
 
-class FriendlychatApp extends StatelessWidget {
+class FriendlyChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -14,19 +14,36 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-//class ChatScreen extends StatelessWidget {
-//  @override
-//  State createState() => new ChatScreenState();
-//}
-//
-//class ChatScreenState extends State<ChatScreen> {
-//  @override
-//  Widget build(BuildContext context) {
-//    return new Scaffold(
-//      appBar: new AppBar(title: new Text("Friendlychat")),
-//    );
-//  }
-//}
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Friendlychat")),
+
+      body: _buildTextComposer(),
+    );
+  }
+  Widget _buildTextComposer() {
+    return new Container(
+      margin: const EdgeInsets.symmetric(horizontal:8.0),
+      child: new TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: new InputDecoration.collapsed(
+            hintText: "Send a message"),
+      ),
+    );
+  }
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+}
 
 
 
